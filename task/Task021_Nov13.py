cd1e2fcebc31bac01eef1015ebd0dae234705808
archanasreekumar <sarchanasreekumar@gmail.com>
2025-11-14 14:21:24 +0530

    Loops

---
+++ task/Task021_Nov13.py
@@ -0,0 +1,5 @@
+"""
+Create a Person class where we will have five attributes and five behaviors.
+Make sure that each type of function is used, and I want you to also create
+the print function, which will print all the instance variable values.
+"""
---
+++ task/Task022_nov13.py
@@ -0,0 +1,6 @@
+"""
+You need to create a calculator function, but the calculator function has to
+take the value from the parameterized constructor. So while creating the object,
+you will pass the parameters and that will basically return the sum of the two numbers,
+multiplication of two numbers.
+"""
