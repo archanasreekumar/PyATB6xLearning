bbe08438e736cddf34eb7cee1a230488000ddf13
archanasreekumar <sarchanasreekumar@gmail.com>
2025-11-14 18:44:36 +0530

    Task nov13

--- task/Task021_Nov13.py
+++ task/Task021_Nov13.py
@@ -3,3 +3,33 @@ Create a Person class where we will have five attributes and five behaviors.
 Make sure that each type of function is used, and I want you to also create
 the print function, which will print all the instance variable values.
 """
+
+class Person:
+    def __init__(self):
+        self.name = "Archana"
+        self.age = 31
+        self.height = "5 ft"
+        self.weight = 53
+        self.eye_color = "Black"
+
+    def person_name(self):
+        return self.name
+
+    def person_age(self):
+        return self.age
+
+    def person_height(self):
+        return self.height
+
+    def person_weight(self):
+        return self.weight
+
+    def person_eye_color(self):
+        return self.eye_color
+
+person = Person()
+print(person.person_name())
+print(person.person_age())
+print(person.person_height())
+print(person.person_weight())
+print(person.person_eye_color())
--- task/Task022_nov13.py
+++ task/Task022_nov13.py
@@ -4,3 +4,12 @@ take the value from the parameterized constructor. So while creating the object,
 you will pass the parameters and that will basically return the sum of the two numbers,
 multiplication of two numbers.
 """
+
+class Calc:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+        print(self.a + self.b)
+        print(self.a * self.b)
+
+c = Calc(5, 6)
